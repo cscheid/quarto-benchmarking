00ca5f6beb05562a181f5fd048ab82c87df727c8
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-08-30 15:10:29 -0700

    filter updates

--- _supporting_docs/filters/readonlypureluawalker.lua
+++ _supporting_docs/filters/readonlypureluawalker.lua
@@ -5,7 +5,7 @@ function Meta(meta)
 end
 
 function pandoc_type(el)
-    local result = el.t
+    local result = el.tag
     if result == "table" or result == nil then
         return pandoc.utils.type(el)
     end
@@ -17,28 +17,43 @@ function lua_readonly_walk(el, filter)
     if filter[t] then
         filter[t](el)
     end
+    local n = 0
+    local c = nil
     if t == "Blocks" or t == "Inlines" then
-        for i = 1, #el do
-            lua_readonly_walk(el[i], filter)
-        end
+        c = el
+        n = #el
     elseif el.content then
-        for i = 1, #el.content do
-            lua_readonly_walk(el.content[i], filter)
-        end
+        c = el.content
+        n = #el.content
+    end
+    for i = 1, n do
+        lua_readonly_walk(c[i], filter)
     end
 end
 
+local profiling = false
+local profiler = dofile('profiler.lua')
 function Pandoc(doc)
+    if profiling then
+        os.execute('rm -f output.prof')
+        profiler.start('output.prof', 1)
+    end
     local c = 0
     for i = 1, n do
+        if profiling then
+            profiler.setcategory('pass-' .. i)
+        end
         lua_readonly_walk(doc.blocks, {
             Str = function(el)
                 c = c + 1
             end
         })
     end
-    -- print(c)
+    print(c)
     doc.blocks = pandoc.Blocks({})
+    if profiling then
+        profiler.stop()
+    end
     return doc
 end
 
